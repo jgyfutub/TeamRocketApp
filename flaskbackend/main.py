9c95eeb66db4d2c479aec99a9bc493d2e006b398
Vedant Pandey <vedantpandey459@gmail.com>
2024-04-20 02:35:37 +0530

    audio

--- flaskbackend/main.py
+++ flaskbackend/main.py
@@ -9,10 +9,39 @@ from collections import Counter
 import numpy as np
 from flask_cors import CORS
 import os
+from scipy.io import wavfile
 from PIL import Image
+import csv
+import scipy
+
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+
+
+def class_names_from_csv(class_map_csv_text):
+  class_names = []
+  with tf.io.gfile.GFile(class_map_csv_text) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      class_names.append(row['display_name'])
+
+  return class_names
+
+def ensure_sample_rate(original_sample_rate, waveform,
+                       desired_sample_rate=16000):
+  if original_sample_rate != desired_sample_rate:
+    desired_length = int(round(float(len(waveform)) /
+                               original_sample_rate * desired_sample_rate))
+    waveform = scipy.signal.resample(waveform, desired_length)
+  return desired_sample_rate, waveform
+
+
+soundmodel = tfhub.load('https://www.kaggle.com/models/google/yamnet/TensorFlow2/yamnet/1')
+
+class_map_path = soundmodel.class_map_path().numpy()
+class_names = class_names_from_csv(class_map_path)
+
 
 @app.route('/upload_video', methods=['POST'])
 def upload_video():
@@ -31,7 +60,20 @@ def upload_audio():
     byte_array = bytearray(blob_data)
     with open("audio.wav", 'wb') as f:
         f.write(byte_array)
-    return jsonify({"message":"done"})
+    sample_rate,wav_data = wavfile.read("audio.wav")
+    if np.ndim(np.array(wav_data))==2:
+       wav_data=[int(sum(i)/len(i)) for i in wav_data]
+    sample_rate, wav_data = ensure_sample_rate(sample_rate, wav_data)
+    waveform = wav_data / tf.int16.max
+    scores, _,_ =soundmodel(waveform)
+    scores_np = scores.numpy()
+    infered_class = class_names[scores_np.mean(axis=0).argmax()]
+    print(infered_class)
+    mean_scores = np.mean(scores, axis=0)
+    top_n = 10
+    top_class_indices = np.argsort(mean_scores)[::-1][:top_n]
+    arr=[class_names[top_class_indices[x]] for x in range(0, top_n, 1)]
+    return jsonify({"message":"done","result":arr})
 
 @app.route('/upload_image', methods=['POST'])
 def image_upload():
--- frontend/src/Pages/soundclassifier.js
+++ frontend/src/Pages/soundclassifier.js
@@ -7,7 +7,7 @@ export default function SoundClassifier(){
     const media=useRef(null)
 	const [Status, setStatus]=useState("inactive")
 	const [stream, setStream]=useState(null)
-	const [Chunks, setAudioChunks]=useState([])
+	const [Chunks, setChunks]=useState([])
 	const [audio, setAudio]=useState(null)
 	const [result,setresult]=useState([])
     const mimeType ="audio/wav"
@@ -36,12 +36,90 @@ export default function SoundClassifier(){
         }
     }
 
+    const getMicrophonePermission = async () => {
+		if ("MediaRecorder" in window) {
+			try {
+				const mediaStream = await navigator.mediaDevices.getUserMedia({
+					audio: true,
+					video: false,
+				});
+				setPermission(true);
+				setStream(mediaStream);
+			} catch (err) {
+				alert(err.message);
+			}
+		} else {
+			alert("The MediaRecorder API not supported in your browser.");
+		}
+	};
+
+	const startRecording = async () => {
+		setStatus("recording");
+		const media = new MediaRecorder(stream, { type: mimeType });
+		media.current = media;
+		media.current.start();
+		let localAudioChunks = [];
+		media.current.ondataavailable = (event) => {
+			if (typeof event.data === "undefined") return;
+			if (event.data.size === 0) return;
+			localAudioChunks.push(event.data);
+		};
+		setChunks(localAudioChunks);
+	};
+
+	const stopRecording = () => {
+		setStatus("inactive");
+		media.current.stop();
+		media.current.onstop = async () => {
+			const audioBlob = new Blob(Chunks, { type: mimeType });
+			const audioUrl = URL.createObjectURL(audioBlob);
+			const formData = new FormData();
+            formData.append('audio',audioBlob);
+			formData.append('url',audioUrl)
+			setAudio(audioUrl);
+            const result=await axios.post('http://127.0.0.1:5000/upload_audio', formData)
+			setChunks([]);
+		};
+	};
+
+
     return (
         <div>
+        <h2>Audio Recorder</h2>
+				<div className="audio-controls">
+					{!permission ? (
+						<button onClick={getMicrophonePermission} type="button">
+							Get Microphone
+						</button>
+					) : null}
+					{permission && Status === "inactive" ? (
+						<button onClick={startRecording} type="button">
+							Start Recording
+						</button>
+					) : null}
+					{Status === "recording" ? (
+						<button onClick={stopRecording} type="button">
+							Stop Recording
+						</button>
+					) : null}
+				</div>
         <div>
                 <input type="file" onChange={handleFileChange} />
                 <button onClick={handleUpload}>Upload an WAV file</button>
         </div>
+        {
+					result.length!=0?(
+						<div>
+						<p>The main sound is : {result[0]}</p>
+						<br/>
+						<p>the other possibilities are: </p>
+						<ul>
+					{result.slice(1).map((item, index) => (
+					<li key={index}>{item}</li>
+					))}
+				</ul>
+	   </div>
+					):<div></div> } 
         </div>
     )
 }
